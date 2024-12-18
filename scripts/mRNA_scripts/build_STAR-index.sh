da3569b9cb5360a24308182ded35d35f8d7790c8
Tatjana Kiseļova <147719161+tkiselova@users.noreply.github.com>
2024-12-18 11:50:20 +0200

    comment editing and renaming

--- scripts/mRNA_scripts/build_STAR-index.sh
+++ scripts/mRNA_scripts/build_STAR-index.sh
@@ -2,30 +2,22 @@
 
 # This script generates a STAR genome index from a reference genome FASTA file and a GTF annotation file.
 # Usage:
-# bash 02-1_build_STAR-index.sh <genome_fasta> <gtf_file> <output_directory>
+# bash build_STAR-index.sh <genome_fasta> <gtf_file>
 
 # Check if the correct number of arguments are provided
-if [ "$#" -ne 3 ]; then
-    echo "Usage: $0 <genome_fasta> <gtf_file> <output_directory>"
+if [ "$#" -ne 2 ]; then
+    echo "Usage: $0 <genome_fasta> <gtf_file>"
     exit 1
 fi
 
 # Assign command-line arguments to variables
 GENOME_FASTA=$1       # Path to reference genome FASTA file
 GTF_FILE=$2           # Path to GTF annotation file
-OUTDIR=$3             # Directory where the STAR index files will be saved
-
-# Create output directory if it doesn't exist
-mkdir -p $OUTDIR
-chmod a+rwx $OUTDIR
-
-# Navigate to the output directory
-cd $OUTDIR
 
 # Generate the STAR genome index
 echo "Generating STAR genome index..."
 STAR --runMode genomeGenerate \
-     --genomeDir $OUTDIR \
+     --genomeDir . \
      --genomeFastaFiles $GENOME_FASTA \
      --sjdbGTFfile $GTF_FILE \
      --sjdbOverhang 149 \
@@ -35,4 +27,4 @@ STAR --runMode genomeGenerate \
 chmod a+rw *
 
 # Completion message
-echo "STAR genome index generation complete! Files are stored in $OUTDIR"
+echo "STAR genome index generation complete!"
