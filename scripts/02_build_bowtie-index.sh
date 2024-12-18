bfcbc28d2736d5e9259ebafd4991a1e5d2336d21
Tatjana Kiseļova <147719161+tkiselova@users.noreply.github.com>
2024-12-18 11:20:20 +0200

    changes to comments

--- scripts/02_build_bowtie-index.sh
+++ scripts/02_build_bowtie-index.sh
@@ -2,28 +2,17 @@
 
 # This script builds a Bowtie index from a reference genome FASTA file.
 # Usage:
-# bash 02_build_bowtie-index.sh <genome_fasta> <index_name> <output_directory>
+# bash 02_build_bowtie-index.sh <genome_fasta> <output_name>
 
 # Check if the correct number of arguments are provided
-if [ "$#" -ne 3 ]; then
-    echo "Usage: $0 <genome_fasta> <index_name> <output_directory>"
+if [ "$#" -ne 2 ]; then
+    echo "Usage: $0 <genome_fasta> <output_name>"
     exit 1
 fi
 
 # Assign command-line arguments to variables
 GENOME_FASTA=$1       # Path to reference genome FASTA file
 INDEX_NAME=$2         # Name for the Bowtie index
-OUTDIR=$3            # Directory where the Bowtie index files will be saved
-
-# Create output directory if it doesn't exist
-mkdir -p $OUTDIR
-chmod a+rwx $OUTDIR
-
-# Copy the reference genome to the working directory
-# This ensures that Bowtie can access the genome file.
-cp $GENOME_FASTA $OUTDIR
-GENOME_FILE=$(basename "$GENOME_FASTA")
-cd $OUTDIR
 
 # Build the Bowtie index
 # This command creates an index from the reference genome FASTA file.
@@ -31,4 +20,4 @@ echo "Building Bowtie index..."
 bowtie-build $GENOME_FILE $INDEX_NAME
 
 # Completion message
-echo "Bowtie index built successfully! Index files are stored in $OUTDIR"
+echo "Bowtie index built successfully!"
