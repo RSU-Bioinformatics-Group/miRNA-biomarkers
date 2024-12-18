cea68c9f7cf454d7cfc7ff8b5d7ddfdbd6cfc6ec
Tatjana Kiseļova <147719161+tkiselova@users.noreply.github.com>
2024-12-18 14:21:48 +0200

    updating comments

--- scripts/miRNA_scripts/02_runmirDeep2.sh
+++ scripts/miRNA_scripts/02_runmirDeep2.sh
@@ -19,43 +19,27 @@ INPATH=$1               # Path to input FASTQ files
 OUTPATH=$2              # Path to output directory for mapped reads
 BOWTIE_INDEX_PATH=$3    # Path to Bowtie index directory
 REFERENCE_GENOME=$4     # Path to reference genome FASTA file
-MATURE_MIRNA_FILE=$5    # Path to mature miRNA reference file
-HAIRPIN_MIRNA_FILE=$6   # Path to hairpin miRNA reference file
+MATURE_MIRNA_FILE=$5    # Path to mature miRNA reference file which was previously modified as indicated in README
+HAIRPIN_MIRNA_FILE=$6   # Path to hairpin miRNA reference file which was previously modified as indicated in README
 
 # Step 1: Create output directory if it doesn't exist
 mkdir -p $OUTPATH
 chmod a+rwx $OUTPATH
 
 # Step 2: Run Bowtie mapper on all FASTQ files in the input directory
-for file in $INPATH/*.fq ; do
+for file in $INPATH/*.{fq,fastq,fq.gz,fastq.gz}; do
     bname=$(basename $file '.fq')
     echo "Running Bowtie mapper on $file..."
     mapper.pl $file -e -h -i -j -k AAGTCGGAGGCCAAGCGGTCTTAGGAAGACAA -l 18 -m -p $BOWTIE_INDEX_PATH -s $OUTPATH/$bname.fa -t $OUTPATH/$bname._vs_hsa.arf -v -o 16
 done
 
-# Step 3: Prepare files for miRDeep2 processing
-# Setting locale environment variables
-export LC_ALL=en_US.UTF-8
-export LC_CTYPE=en_US.UTF-8 
-
-# Clean up formatting issues in reference files
-echo "Cleaning up reference files..."
-perl -plane 's/\s+.+$//' < $REFERENCE_GENOME > ${REFERENCE_GENOME%.fa}_cleaned.fa
-perl -plane 's/\s+.+$//' < $MATURE_MIRNA_FILE > ${MATURE_MIRNA_FILE%.fa}_cleaned.fa
-perl -plane 's/\s+.+$//' < $HAIRPIN_MIRNA_FILE > ${HAIRPIN_MIRNA_FILE%.fa}_cleaned.fa
-
-# Extract miRNAs from cleaned reference files
-echo "Extracting miRNAs from reference files..."
-extract_miRNAs.pl ${MATURE_MIRNA_FILE%.fa}_cleaned.fa hsa > ${MATURE_MIRNA_FILE%.fa}_extracted.fa
-extract_miRNAs.pl ${HAIRPIN_MIRNA_FILE%.fa}_cleaned.fa hsa > ${HAIRPIN_MIRNA_FILE%.fa}_extracted.fa
-
-# Step 4: Run miRDeep2 on the processed files
+# Step 3: Run miRDeep2 on the processed files
 files=("$OUTPATH"/*.fa)
 for file in "${files[@]}"; do
     echo "Processing file: $file"
     bname=$(basename $file '.fa')
     echo "Running miRDeep2 on $file..."
-    miRDeep2.pl $file $REFERENCE_GENOME $OUTPATH/$bname._vs_hsa.arf ${MATURE_MIRNA_FILE%.fa}_extracted.fa none ${HAIRPIN_MIRNA_FILE%.fa}_extracted.fa -t Human -P -v -z $bname
+    miRDeep2.pl $file $REFERENCE_GENOME $OUTPATH/$bname._vs_hsa.arf $MATURE_MIRNA_FILE none $HAIRPIN_MIRNA_FILE -t Human -P -v -z $bname
 done
 
 # Completion message
